5f772d6f5b8cf0016da52c75307aeba78f41c144
Stephane.C <tchadizman@gmail.com>
2019-10-21 12:28:53 +0200

    Creation de Command Controlleur WIP

---
+++ FruitStocker/Controllers/CommandeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FruitStockerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandeController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CommandFruit(Guid fruitLot, string clientName, int quantity)
+        {
+            // Vériier si le lot n'est pas périmé
+
+            // Vérifier si la quantité restante est suffisante
+
+            // Créer la commande
+
+            throw new NotImplementedException("Construction en cour");
+        }
+
+    }
+}
