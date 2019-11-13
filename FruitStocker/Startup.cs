826a8fdc5bb69adb6a72024072f1324e3f3b7648
Stéphane <stephane.chapiteau-ext@fnacdarty.com>
2019-11-13 17:09:11 +0100

    Modification du startUp

--- FruitStocker/Startup.cs
+++ FruitStocker/Startup.cs
@@ -55,7 +55,7 @@ namespace FruitStocker
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FruitLot API");
             });
 
             app.UseHttpsRedirection();
